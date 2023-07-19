519d8fd5bfcc4e40455d1cbc7d1c2fd416a1fddb
Filipe Barros <tiagosoarez6@gmail.com>
2023-07-19 13:27:50 +0100

    section 13 fim

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -7,6 +7,7 @@ use App\Models\Address;
 use App\Models\Category;
 use App\Models\MyPost;
 use App\Models\Post;
+use App\Models\Tag;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
@@ -20,8 +21,24 @@ class HomeController extends Controller
     {
         /* $categories = Category::all(); */
         
-        $categories = Category::find(2)->posts;
+        /* $categories = Category::find(2)->posts;
 
-        return view('home', compact('categories'));
+        return view('home', compact('categories')); */
+
+        // post may have many tags
+        
+        // tag may have many posts
+
+        //pivot table
+
+        $posts = Post::with('tags')->get();
+
+        $tag = Tag::first();
+
+        //$post->tags()->attach($tag);
+        //$post->tags()->attach([2,3,4]);
+
+        //return $post;
+        return view('home', compact('posts'));
     }
 }
